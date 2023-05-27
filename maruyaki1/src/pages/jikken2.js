05e8c389f0e631aec322140f5340f0bff48144b4
sohmaono <sohmaono@keio.jp>
2023-05-27 14:48:54 +0900

    あdsf

--- maruyaki1/src/pages/jikken2.js
+++ maruyaki1/src/pages/jikken2.js
@@ -3,8 +3,12 @@ import style from "./jikken2.module.css"
 export default function (){
     return(
     <>
-        <div className={style.title}>サッカーのサイト</div>
-        <div>野球</div>
+        <div className={style.title}>スポーツ</div>
+        <ti className={style.ti}>
+            <div>野球</div>
+            <div>サッカー</div>
+            <div>ラグビー</div>
+        </ti>
     </>
     )
 }
